fe3a04ff5a3a14dc0f4baaf1637c576c6ca37edf
Brent Groves <brent@robogroves.com>
2017-02-22 19:27:22 -0500

    first commit

---
+++ app/reducers/index.js
@@ -0,0 +1,19 @@
+// @flow
+import { combineReducers } from 'redux';
+import { routerReducer as routing } from 'react-router-redux';
+import counter from './counter';
+import POReqTrans from './production/POReqTrans'
+import Common from './Common'
+import GenReceivers from './production/GenReceivers'
+import ProdReports from './rpt/production/ProdReports'
+
+const rootReducer = combineReducers({
+  counter,
+  Common,
+  POReqTrans,
+  GenReceivers,
+  ProdReports,
+  routing
+});
+
+export default rootReducer;
