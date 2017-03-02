9e02b846ce57246e49229679b98b3e5960432eed
Brent Groves <brent.groves@gmail.com>
2017-03-02 15:24:40 -0500

    0302 Change#1

--- tmp2/renderer/override.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const {ipcRenderer} = require('electron')
-
-const {guestInstanceId, openerId} = process
-const hiddenPage = process.argv.includes('--hidden-page')
-
-require('./window-setup')(ipcRenderer, guestInstanceId, openerId, hiddenPage)
